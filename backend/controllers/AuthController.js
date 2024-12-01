8d593d97c1b5715c87de644e2ed73fedf65ab5bc
gabrielflorianoo <tacacheee221@gmail.com>
2024-11-30 21:19:15 -0300

    criado authcontroller

---
+++ backend/controllers/AuthController.js
@@ -0,0 +1,10 @@
+const AuthController = {
+    async checkAuth(req, res, next) {
+        if (typeof req.session.user === 'undefined') {
+            return res.status(401).json({ message: 'Usuário não autenticado' });
+        }
+        next();
+    },
+};
+
+export default AuthController;
