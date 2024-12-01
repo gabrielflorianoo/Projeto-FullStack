c003c6269ba3b6d85a6ec7a6e42a8fc928e18d8d
gabrielflorianoo <tacacheee221@gmail.com>
2024-12-01 10:40:40 -0300

    criado ConvertorController

--- backend/controllers/ConvertorController.js
+++ backend/controllers/ConvertorController.js
@@ -1,2 +1,33 @@
-// Todos os codigos relacionados a conversão serão colocados aqui
-// TODO
+const { ConverterModel } = require('../db/Models');
+
+const createConverter = async (req, _res) => {
+    const { targetCurrency, exchangeRate } = req.body;
+
+    try {
+        const converter = new ConverterModel({ targetCurrency, exchangeRate });
+        await converter.save();
+
+        return [success, null];
+    } catch (error) {
+        return [null, error];
+    }
+};
+
+const getConverterInPeriod = async (req, _res) => {
+    const { startDate, endDate } = req.body;
+
+    try {
+        const converters = await ConverterModel.find({
+            createdAt: { $gte: startDate, $lt: endDate },
+        });
+
+        return [converters, null];
+    } catch (error) {
+        return [null, error];
+    }
+}
+
+module.exports = {
+    createConverter,
+    getConverterInPeriod,
+}
--- backend/db/Models.js
+++ backend/db/Models.js
@@ -15,13 +15,47 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    history: [{
+        type: mongoose.Schema.Types.ObjectId, // Referência ao modelo Converter
+        ref: 'Conversion', // Nome do modelo Converter
+    }],
 }, {
     timestamps: true
 });
 
+const converterSchema = new mongoose.Schema({
+    targetCurrency: { // Moeda alvo
+        type: String,
+        required: true,
+        trim: true
+    },
+    exchangeRate: { // Taxa de câmbio
+        type: Number,
+        required: true
+    },
+    sourceCurrency: { // Moeda usada para o câmbio
+        type: String,
+        required: true,
+        trim: true
+    },
+    amountUsed: { // Valor usado na conversão
+        type: Number,
+        required: true
+    },
+    conversionDate: { // Data da conversão
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true // Garante que o registro tenha createdAt e updatedAt
+});
+
+// Criação dos modelos baseados nos schemas
 const UserModel = mongoose.model('usuarios', userSchema);
+const ConverterModel = mongoose.model('Conversion', converterSchema);
 
 module.exports = {
-    UserModel
+    UserModel,
+    ConverterModel,
 };
