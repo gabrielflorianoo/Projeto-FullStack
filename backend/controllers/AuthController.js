5ad728e2b88510662750f12919f29a944a1dc789
robsonldcarvalho <143975321+robsonldcarvalho@users.noreply.github.com>
2025-02-12 09:30:09 -0300

    Implementação de tokens JWT autenticação em tokens

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,9 +1,19 @@
+const jwt = require('jsonwebtoken');
+
 const AuthController = {
     async checkAuth(req, res, next) {
-        if (typeof req.session.userId === 'undefined') {
+        const token = req.headers.authorization;
+        if (!token) {
             return res.status(401).json({ message: 'Usuário não autenticado' });
         }
-        next();
+        
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.userId = decoded.id;
+            next();
+        } catch (error) {
+            return res.status(401).json({ message: 'Token inválido' });
+        }
     },
 };
 
