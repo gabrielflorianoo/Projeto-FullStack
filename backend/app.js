f62aa815ce9f2357e0439a7ab149f920a37bed31
gabrielflorianoo <tacacheee221@gmail.com>
2025-02-21 20:32:55 -0300

    Ajustada a conexão com o cliente Redis para incluir mensagens de sucesso e erro

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,9 @@ const converterRouter = require('./routes/conversion');
 
 const redisClient = redis.createClient({ url: process.env.REDIS_URL });
 
-redisClient.on('error', (err) => console.error('Erro no Redis:', err));
-redisClient.connect();
+redisClient.connect()
+    .then(() => console.log("✅ Redis conectado com sucesso!"))
+    .catch((err) => console.error("❌ Erro ao conectar ao Redis:", err));
 
 require('./db/server');
 
